9c3c07600b08abc0e87a6398d0f037618094d9c6
Hiten Chowdhary <hiten.iitkgp@gmail.com>
2015-07-16 20:25:16 +0530

    READ operation added

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,10 @@ router.post('/api/v1/todos', function (req, res) {
 	pg.connect(connectionString, function(err, client, done) {
 
 		// SQL query > Insert data
-		client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+		client.query("INSERT INTO items (text, complete) VALUES ($1, $2);", [data.text, data.complete]);
 
 		// SQL Query > Select data
-		var query = client.query("SELECT * FROM items ORDER BY id ASC");
+		var query = client.query("SELECT * FROM items ORDER BY id ASC;");
 
 		// Stream results back one row at a time
 		query.on('row', function(row) {
@@ -31,13 +31,42 @@ router.post('/api/v1/todos', function (req, res) {
 		});
 
 		// After all data is returned close the connection and return the results
-		query.on('end', fucntion() {
+		query.on('end', function() {
 			client.end();
 			return res.json(results);
 		});
 
 		// Handle errors
 		if (err) {
+			console.log(err);
+		}
+	});
+});
+
+/* READ Data or todos from items table */
+router.get('/api/v1/todos', function (req, res) {
+
+	var results = [];
+
+	// Get a postgres client from the connection pool
+	pg.connect(connectionString, function (err, client, done) {
+
+		// SQL query > Select data
+		var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+		// Send the data one row at a time
+		query.on('row', function(row) {
+			results.push(row);
+		});
+
+		// After all data is returned back, close the connection and return the results
+		query.on('end', function() {
+			client.end();
+			return res.json(results)
+		});
+
+		// Handle errors
+		if(err) {
 			console.log(err);
 		}
 	});
